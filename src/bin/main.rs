a74e99fa6673b0dda2bc3f0183f43667eb72565c
Kreyren <KostWarCZE@RiXotStudio.cz>
2020-03-03 18:47:29 +0000

    Stub for today

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -14,24 +14,27 @@ use std::fs::OpenOptions;
 use std::process::exit;
 use structopt::StructOpt;
 
-use os_detect::detect_os_from_path;
-//use os_detect::detect_windows;
 use std::path::Path;
+use os_detect::detect_os_from_path;
+//use os_detect::detect_windows; -- Stubbed
 
 fn main() {
     // Kernel detection
-    // FIXME: export in lib.rs?
-    // FIXME-QA: Add test/bench?
-    if cfg!(target_os = "linux") || cfg!(target_os = "macos") {
-        detect_os_from_path(Path::new("/"));
+    // This exports OS enum with following data https://docs.rs/os-detect/0.2.2/os_detect/enum.OS.html
+    // FIXME: Export in lib.rs so that this can be used as a crate on demand
+    // FIXME-bench: I assume that implementing it this way is the most efficient? Bench required -- Krey
+    if cfg!(target_os = "unix") {
+        detect_os_from_path(Path::new("/"))
+          .expect("Failed to detect os from path, unsupported kernel?");
     } else if cfg!(target_os = "windows") {
+        // FIXME-QA: Use die() ?
         println!("Windows is currently not supported, fixme?");
         exit(1);
         //detect_windows(Path::new("c:/"));
     } else {
-      // FIXME: Output the kernel
-      println!("This {} kernel is not supported\n", "FIXME_KERNEL");
-      exit(255);
+        // FIXME: Output the kernel
+        println!("This {:?} kernel is not supported\n", "FIXME_DETECT_OS");
+        exit(255);
     }
 
     let args = Cli::from_args();

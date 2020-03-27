88a0d1b63d7c81f85f1e5285a2aa3017dc0e4074
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-03-28 03:59:09 +0600

    Removed All Possible Bloats (#68)
    * Removed All Possible Bloats
    
    * rearranged the name
    
    * added Main Function
    
    The main function is added to prevent (certain) code from being run when the module is imported

--- gui/gui.py
+++ gui/gui.py
@@ -1,44 +1,50 @@
 import PySimpleGUI as sg
 from os import system
 from os.path import exists
+from termcolor import cprint
 
 # Theme
 sg.theme('DarkAmber')
 
-layout = [
-    [sg.Text('Welcome to the DockerFreeze GUI, written in Python.')],
-    # [sg.Text('Add Environment Variables', size=(23, 1)), sg.Checkbox("", default=False)],
-    # [sg.Text('     Pretty Print Dockerfile', size=(23, 1)), sg.Checkbox("", default=False)],
-    # [sg.Text('      Install user packages', size=(23, 1)), sg.Checkbox("", default=False)],
-    [sg.Text('           Gitpod Ready', size=(23, 1)),
-     sg.Checkbox("", default=False)],
-    # [sg.Text('              Vagrant', size=(23, 1)), sg.Checkbox("", default=False)],
-    [sg.Submit("BUILD"), sg.Cancel()]]
+
+def warn(error):
+    cprint(f"Error: {error}", "red")
+    sg.popup(f"Error: {error}", title="Attention!")
 
 
-window = sg.Window('Docker Freeze Gui',
-                   default_element_size=(40, 1)).Layout(layout)
-while True:
-    button, values = window.Read()
-    if button in (None, "Cancel"):
-        break
-    elif button == "BUILD":
-        did_error = False
-        if exists("Dockerfile"):
-            sg.popup("Error: File already exists")
-            continue
-        if values[0]:
-            if system("dockerfreeze -g &> /dev/null") != 0:
-                did_error = True
-        else:
-            if system("dockerfreeze &> /dev/null") != 0:
-                did_error = True
-        if did_error:
-            sg.popup("Error: File already exists")
-        else:
-            sg.popup("Dockerfile Built")
-        did_error = False
+def main():
+    layout = [
+        [sg.Text('Welcome To Dockerfreeze GUI')],
+        [sg.T(" " * 7), sg.Text('Gitpod Ready', size=(12, 1)), sg.Checkbox("", default=False)],
+        [sg.T(" " * 3), sg.Submit("BUILD"), sg.Cancel()]
+    ]
 
-if __name__ == '__main__':
-    print("Starting...")
-    print(button, values[0:])
+    mainWindow = sg.Window('Dockerfreeze GUI', default_element_size=(40, 1)).Layout(layout)
+    cprint("Main Window Running", "green")
+    
+    while True:  # Event Loop
+        button, values = mainWindow.Read()
+        if button in ("Cancel", None):
+            break
+        if button == "BUILD":
+            did_error = False
+            if exists("Dockerfile"):
+                warn("File already exists")
+                continue
+            if values[0]:
+                if system("dockerfreeze -g &> /dev/null") != 0:
+                    did_error = True
+            else:
+                if system("dockerfreeze &> /dev/null") != 0:
+                    did_error = True
+            if did_error:
+                warn("File already exists")
+            else:
+                sg.popup("Dockerfile Built")
+            did_error = False
+    cprint("Closing Application...", "red")
+    mainWindow.close()
+
+
+if __name__ == "__main__":
+    main()
